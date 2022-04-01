5c2f14cd4e166226d955ab6bfcb7b5829477d4f7
sal64 <salazar_64@live.com>
2022-04-01 15:36:41 -0500

    add line graph

--- graphs/bokehApp/models.py
+++ graphs/bokehApp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
+class Hospitalizations(models.Model):
+    HospiWeeks = models.IntegerField()
+    HospiPati = models.IntegerField()
+
+    def __str__(self):
+        return "{}-{}.format(self.HospiWeeks self.HospiPati)"
+# Create your models here.
+
+
 # Create your models here.
--- graphs/bokehApp/views.py
+++ graphs/bokehApp/views.py
@@ -1,14 +1,45 @@
+from itertools import count
 from django.shortcuts import render
 from bokeh.plotting import figure
 from bokeh.embed import components
-from bokeh.models import HoverTool
+from bokeh.models import HoverTool, LassoSelectTool, WheelZoomTool, PointDrawTool, ColumnDataSource
 
+from bokeh.palettes import Category20c, Spectral6
+from bokeh.transform import cumsum
+from .models import Hospitalizations
+from bokeh.resources import CDN
 
-def starter(request):
+"""
     plot = figure()
     plot.circle([1, 10, 35, 27], [0, 0, 0, 0], size = 20, color = "blue")
 
     script, div = components(plot)
+"""
+#=======================================================================================
+
+def starter(request):
+    shoes = 0
+    belts = 0
+    shirts = 0
+    counts = []
+
+    items = ["Shoes", "Belts", "Shirts"]
+    patients = (Hospitalizations.objects.values_list('HospiPati', flat=True))
+    weeks = (Hospitalizations.objects.values_list('HospiWeeks', flat=True))
 
 
-    return render(request, 'starter.html', {'script': script, 'div': div})
+
+    plot = figure( plot_height=600, plot_width=600, title="Products",
+           toolbar_location="right", tools="pan,wheel_zoom,box_zoom,reset, hover, tap, crosshair")
+    plot.title.text_font_size = '20pt'
+    
+    plot.xaxis.major_label_text_font_size = "14pt" 
+    plot.line(weeks, patients)
+    plot.legend.label_text_font_size = '14pt'
+
+    script, div = components(plot)
+    
+    #return render(request, 'starter.html', {'script': script, 'div': div})
+
+
+    return render(request, 'starter.html' , {'script': script, 'div':div})
